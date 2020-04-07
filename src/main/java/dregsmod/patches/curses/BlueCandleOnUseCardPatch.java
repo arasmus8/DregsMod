c8f36494da7af72fbaec0236e0a67b4f0d5435d9
arasmus8 <arasmus8@gmail.com>
2020-04-07 09:03:11 -0600

    fix: blue candle interaction

--- src/main/java/dregsmod/patches/curses/BlueCandleOnUseCardPatch.java
+++ src/main/java/dregsmod/patches/curses/BlueCandleOnUseCardPatch.java
@@ -6,6 +6,8 @@ import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.BlueCandle;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import dregsmod.cards.curses.Jealousy;
 import dregsmod.cards.rare.EclipseForm;
 
 @SpirePatch(
@@ -15,6 +17,15 @@ import dregsmod.cards.rare.EclipseForm;
 public class BlueCandleOnUseCardPatch {
     public static SpireReturn Prefix(BlueCandle _instance, AbstractCard card, UseCardAction action) {
         if (AbstractDungeon.player.hasPower(EclipseForm.ID)) {
+            return SpireReturn.Return(null);
+        } else if (card.costForTurn >= 0 && card.costForTurn <= EnergyPanel.getCurrentEnergy()) {
+            if (card.cardID.equals(Jealousy.ID)) {
+                if (((Jealousy) card).holdingCurse) {
+                    return SpireReturn.Return(null);
+                } else {
+                    return SpireReturn.Continue();
+                }
+            }
             return SpireReturn.Return(null);
         }
         return SpireReturn.Continue();
