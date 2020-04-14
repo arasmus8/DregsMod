21df51bef5dadfc054ff333cce7d235f38858e14
arasmus8 <arasmus8@gmail.com>
2020-04-14 17:32:16 -0600

    fix(ui): chaos theory icon

--- src/main/java/dregsmod/powers/ChaosTheoryPower.java
+++ src/main/java/dregsmod/powers/ChaosTheoryPower.java
@@ -1,6 +1,8 @@
 package dregsmod.powers;
 
 import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -9,6 +11,7 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import dregsmod.DregsMod;
+import dregsmod.util.TextureLoader;
 
 public class ChaosTheoryPower extends AbstractPower implements CloneablePowerInterface {
 
@@ -16,6 +19,7 @@ public class ChaosTheoryPower extends AbstractPower implements CloneablePowerInt
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    private static final Texture texture = TextureLoader.getTexture(DregsMod.makePowerPath("chaostheory.png"));
 
     public ChaosTheoryPower(AbstractPlayer owner, int amount) {
         name = NAME;
@@ -23,7 +27,8 @@ public class ChaosTheoryPower extends AbstractPower implements CloneablePowerInt
         this.owner = owner;
         this.amount = amount;
 
-        loadRegion("mayhem");
+        region48 = new TextureAtlas.AtlasRegion(texture, 0, 0, 32, 32);
+        region128 = new TextureAtlas.AtlasRegion(texture, 0, 0, 32, 32);
         updateDescription();
     }
 
