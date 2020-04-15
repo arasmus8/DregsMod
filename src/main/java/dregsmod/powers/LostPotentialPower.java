a432c7eb221f5645964e6a4ad07ca72ac5726f15
arasmus8 <arasmus8@gmail.com>
2020-04-15 17:09:08 -0600

    fix(ui): lost potential icon

--- src/main/java/dregsmod/powers/LostPotentialPower.java
+++ src/main/java/dregsmod/powers/LostPotentialPower.java
@@ -1,6 +1,8 @@
 package dregsmod.powers;
 
 import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.actions.defect.ChannelAction;
 import com.megacrit.cardcrawl.actions.defect.IncreaseMaxOrbAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -10,6 +12,7 @@ import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.orbs.Lightning;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import dregsmod.DregsMod;
+import dregsmod.util.TextureLoader;
 
 public class LostPotentialPower extends AbstractPower implements CloneablePowerInterface, TriggerOnSealedPower {
 
@@ -17,6 +20,7 @@ public class LostPotentialPower extends AbstractPower implements CloneablePowerI
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    private static final Texture texture = TextureLoader.getTexture(DregsMod.makePowerPath("lostpotential.png"));
     private static boolean upgraded;
 
     public LostPotentialPower(AbstractPlayer owner, int amount, boolean isUpgraded) {
@@ -30,7 +34,8 @@ public class LostPotentialPower extends AbstractPower implements CloneablePowerI
             upgraded = isUpgraded;
         }
 
-        loadRegion("static_discharge");
+        region48 = new TextureAtlas.AtlasRegion(texture, 0, 0, 32, 32);
+        region128 = new TextureAtlas.AtlasRegion(texture, 0, 0, 32, 32);
         updateDescription();
     }
 
