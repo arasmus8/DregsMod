25f4f6b9322c2473fe9df9bbf909572f65d967a5
arasmus8 <arasmus8@gmail.com>
2020-04-15 15:10:50 -0600

    feat: insight potion

---
+++ src/main/java/dregsmod/potions/InsightPotion.java
@@ -0,0 +1,61 @@
+package dregsmod.potions;
+
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.localization.PotionStrings;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import dregsmod.DregsMod;
+import dregsmod.actions.CardAwokenAction;
+
+public class InsightPotion extends AbstractPotion {
+    public static final String POTION_ID = DregsMod.makeID(InsightPotion.class.getSimpleName());
+    private static final PotionStrings potionStrings = CardCrawlGame.languagePack.getPotionString(POTION_ID);
+
+    public static final String NAME = potionStrings.NAME;
+    public static final String[] DESCRIPTIONS = potionStrings.DESCRIPTIONS;
+
+    public static final Color LIQUID_COLOR = Color.BLACK;
+    public static final Color HYBRID_COLOR = Color.MAROON;
+    public static final Color SPOTS_COLOR = Color.CLEAR;
+
+    public InsightPotion() {
+        super(NAME, POTION_ID, PotionRarity.UNCOMMON, PotionSize.SPHERE, PotionColor.SMOKE);
+        isThrown = false;
+    }
+
+
+    @Override
+    public void use(AbstractCreature abstractCreature) {
+        if (AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
+            for (int i = 0; i < potency; i++) {
+                addToBot(new CardAwokenAction());
+            }
+        }
+    }
+
+    @Override
+    public void initializeData() {
+        potency = getPotency();
+        if (potency > 1) {
+            description = DESCRIPTIONS[0] + potency + DESCRIPTIONS[2];
+        } else {
+            description = DESCRIPTIONS[0] + potency + DESCRIPTIONS[1];
+        }
+        tips.clear();
+        tips.add(new PowerTip(name, description));
+    }
+
+    @Override
+    public int getPotency(int i) {
+        return 1;
+    }
+
+    @Override
+    public AbstractPotion makeCopy() {
+        return new InsightPotion();
+    }
+}
