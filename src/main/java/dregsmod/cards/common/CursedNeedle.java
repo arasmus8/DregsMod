4b6722810754c7e368e176e841a901c5d7e0632c
arasmus8 <arasmus8@gmail.com>
2020-04-16 15:43:44 -0600

    fix(cosmetic): card image

--- src/main/java/dregsmod/cards/common/CursedNeedle.java
+++ src/main/java/dregsmod/cards/common/CursedNeedle.java
@@ -22,7 +22,7 @@ public class CursedNeedle extends CustomCard {
     public static final String ID = DregsMod.makeID(CursedNeedle.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("CursedNeedle.png");
 
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
