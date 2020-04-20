52a5842d3ba266709b633b69479d1bb276e947be
arasmus8 <arasmus8@gmail.com>
2020-04-20 13:44:53 -0600

    fix(cosmetic): card image

--- src/main/java/dregsmod/cards/common/ShadowDagger.java
+++ src/main/java/dregsmod/cards/common/ShadowDagger.java
@@ -21,7 +21,7 @@ public class ShadowDagger extends CustomCard implements UpgradeTextChangingCard 
     public static final String ID = DregsMod.makeID(ShadowDagger.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("ShadowDagger.png");
 
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
