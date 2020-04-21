5aba419e96b3b405f3a13c11db3b94ae6087ec0c
arasmus8 <arasmus8@gmail.com>
2020-04-21 12:39:25 -0600

    fix(cosmetic): card image

--- src/main/java/dregsmod/cards/LashOut.java
+++ src/main/java/dregsmod/cards/LashOut.java
@@ -17,7 +17,7 @@ public class LashOut extends AbstractCurseHoldingCard {
     // TEXT DECLARATION
 
     public static final String ID = DregsMod.makeID(LashOut.class.getSimpleName());
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("LashOut.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
     // Must have an image with the same NAME as the card in your image folder!.
 
