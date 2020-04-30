8989ca74b32484e0470bae65d1e6bcfa60eff517
arasmus8 <arasmus8@gmail.com>
2020-04-30 10:41:43 -0600

    fix(cosmetic): card image

--- src/main/java/dregsmod/cards/rare/ChaosTheory.java
+++ src/main/java/dregsmod/cards/rare/ChaosTheory.java
@@ -15,7 +15,7 @@ import static dregsmod.DregsMod.makeCardPath;
 public class ChaosTheory extends CustomCard {
 
     public static final String ID = DregsMod.makeID(ChaosTheory.class.getSimpleName());
-    public static final String IMG = makeCardPath("Power.png");
+    public static final String IMG = makeCardPath("ChaosTheory.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
 
     private static final CardRarity RARITY = CardRarity.RARE;
