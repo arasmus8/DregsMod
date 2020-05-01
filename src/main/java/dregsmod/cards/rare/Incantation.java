1dd52e0a90af6672dcd3eba9bb9a7ca6553b0d6e
arasmus8 <arasmus8@gmail.com>
2020-05-01 17:50:59 -0600

    fix(cosmetic): card image

--- src/main/java/dregsmod/cards/rare/Incantation.java
+++ src/main/java/dregsmod/cards/rare/Incantation.java
@@ -15,7 +15,7 @@ import static dregsmod.DregsMod.makeCardPath;
 public class Incantation extends CustomCard {
 
     public static final String ID = DregsMod.makeID(Incantation.class.getSimpleName());
-    public static final String IMG = makeCardPath("Power.png");
+    public static final String IMG = makeCardPath("Incantation.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
 
     private static final CardRarity RARITY = CardRarity.RARE;
