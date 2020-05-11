50bcdb7efe01173e17807289c1dd094d3e86bc5e
arasmus8 <arasmus8@gmail.com>
2020-05-11 16:07:07 -0600

    fix(cosmetic): card image

--- src/main/java/dregsmod/cards/rare/Redemption.java
+++ src/main/java/dregsmod/cards/rare/Redemption.java
@@ -17,7 +17,7 @@ public class Redemption extends CustomCard {
     public static final String ID = DregsMod.makeID(Redemption.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("Redemption.png");
 
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
