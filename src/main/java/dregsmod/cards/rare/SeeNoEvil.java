b7b112477e5ab8d6171f8a29bc76da19ad9b40b2
arasmus8 <arasmus8@gmail.com>
2020-05-13 15:51:23 -0600

    fix(cosmetic): card image

--- src/main/java/dregsmod/cards/rare/SeeNoEvil.java
+++ src/main/java/dregsmod/cards/rare/SeeNoEvil.java
@@ -19,7 +19,7 @@ public class SeeNoEvil extends CustomCard implements UpgradeTextChangingCard {
 
     public static final String ID = DregsMod.makeID(SeeNoEvil.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("SeeNoEvil.png");
 // Must have an image with the same NAME as the card in your image folder!
 
 // /TEXT DECLARATION/
