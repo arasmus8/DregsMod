66c19b311977cbebbd8f7c343a638a4d22cad6cf
arasmus8 <arasmus8@gmail.com>
2020-05-13 16:40:03 -0600

    fix(cosmetic): card image

--- src/main/java/dregsmod/cards/rare/Twilight.java
+++ src/main/java/dregsmod/cards/rare/Twilight.java
@@ -18,7 +18,7 @@ public class Twilight extends CustomCard {
     public static final String ID = DregsMod.makeID(Twilight.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("Twilight.png");
 
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
