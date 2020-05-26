8abf361e9417f5761b212e1878f4e92dd3c74c61
arasmus8 <arasmus8@gmail.com>
2020-05-26 10:46:07 -0600

    fix(cosmetic): card image

--- src/main/java/dregsmod/cards/uncommon/TheVault.java
+++ src/main/java/dregsmod/cards/uncommon/TheVault.java
@@ -15,7 +15,7 @@ import static dregsmod.DregsMod.makeCardPath;
 public class TheVault extends CustomCard {
 
     public static final String ID = DregsMod.makeID(TheVault.class.getSimpleName());
-    public static final String IMG = makeCardPath("Power.png");
+    public static final String IMG = makeCardPath("TheVault.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
