226795d3714db6dc0de91f5d5205415ed5fe7d63
arasmus8 <arasmus8@gmail.com>
2020-06-04 13:05:37 -0600

    fix(cosmetic): card image

--- src/main/java/dregsmod/cards/uncommon/BrushOff.java
+++ src/main/java/dregsmod/cards/uncommon/BrushOff.java
@@ -20,7 +20,7 @@ public class BrushOff extends CustomCard {
     public static final String ID = DregsMod.makeID(BrushOff.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("BrushOff.png");
 
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
