85660b7e3403836039ee9bc1fde6e19b54cea5f4
arasmus8 <arasmus8@gmail.com>
2020-06-08 12:03:06 -0600

    fix(cosmetic): card image

--- src/main/java/dregsmod/cards/uncommon/BrutalSwing.java
+++ src/main/java/dregsmod/cards/uncommon/BrutalSwing.java
@@ -22,7 +22,7 @@ public class BrutalSwing extends AbstractCurseHoldingCard {
     public static final String ID = DregsMod.makeID(BrutalSwing.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 
-    public static final String IMG = makeCardPath("Attack.png");
+    public static final String IMG = makeCardPath("BrutalSwing.png");
 
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
