835ec26356097d1f0ddc8602c24ab55cce079198
arasmus8 <arasmus8@gmail.com>
2020-07-15 17:35:50 -0600

    fix(ui): card image

--- src/main/java/dregsmod/cards/uncommon/Scapegoat.java
+++ src/main/java/dregsmod/cards/uncommon/Scapegoat.java
@@ -15,7 +15,7 @@ import static dregsmod.DregsMod.makeCardPath;
 public class Scapegoat extends CustomCard {
 
     public static final String ID = DregsMod.makeID(Scapegoat.class.getSimpleName());
-    public static final String IMG = makeCardPath("Power.png");
+    public static final String IMG = makeCardPath("Scapegoat.png");
     public static CardStrings CARD_STRINGS = languagePack.getCardStrings(ID);
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
