ff957a68f243d6b60624e62515e96968894610c9
arasmus8 <arasmus8@gmail.com>
2020-07-27 17:18:36 -0600

    fix(ui): card image

--- src/main/java/dregsmod/cards/uncommon/EmbraceDarkness.java
+++ src/main/java/dregsmod/cards/uncommon/EmbraceDarkness.java
@@ -21,7 +21,7 @@ public class EmbraceDarkness extends AbstractCurseHoldingCard {
 
     public static final String ID = DregsMod.makeID(EmbraceDarkness.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("EmbraceDarkness.png");
 // Must have an image with the same NAME as the card in your image folder!
 
 // /TEXT DECLARATION/
