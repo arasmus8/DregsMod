8326b7d4ee722714f514b5029b05019b4026a151
arasmus8 <arasmus8@gmail.com>
2020-07-27 16:47:32 -0600

    fix(ui): card image

--- src/main/java/dregsmod/cards/uncommon/CursedScroll.java
+++ src/main/java/dregsmod/cards/uncommon/CursedScroll.java
@@ -20,7 +20,7 @@ public class CursedScroll extends CustomCard {
 
     public static final String ID = DregsMod.makeID(CursedScroll.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("CursedScroll.png");
 // Must have an image with the same NAME as the card in your image folder!
 
 // /TEXT DECLARATION/
