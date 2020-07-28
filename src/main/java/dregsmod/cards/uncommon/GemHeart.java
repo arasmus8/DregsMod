65b93696b5bdfc369b2781ca92076119fad07013
arasmus8 <arasmus8@gmail.com>
2020-07-28 10:50:02 -0600

    fix(ui): card image

--- src/main/java/dregsmod/cards/uncommon/GemHeart.java
+++ src/main/java/dregsmod/cards/uncommon/GemHeart.java
@@ -26,7 +26,7 @@ public class GemHeart extends CustomCard implements UpgradeTextChangingCard {
 
     public static final String ID = DregsMod.makeID(GemHeart.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("GemHeart.png");
 // Must have an image with the same NAME as the card in your image folder!
 
 // /TEXT DECLARATION/
