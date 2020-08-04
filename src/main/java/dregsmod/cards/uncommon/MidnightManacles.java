658ae188ec0103226715de20dfa248c6467631a9
arasmus8 <arasmus8@gmail.com>
2020-08-04 11:59:50 -0600

    fix(ui): card image

--- src/main/java/dregsmod/cards/uncommon/MidnightManacles.java
+++ src/main/java/dregsmod/cards/uncommon/MidnightManacles.java
@@ -20,7 +20,7 @@ public class MidnightManacles extends CustomCard {
 
     public static final String ID = DregsMod.makeID(MidnightManacles.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("MidnightManacles.png");
 // Must have an image with the same NAME as the card in your image folder!
 
 // /TEXT DECLARATION/
