c58f6d9542295919f336a37e501358ac19414436
arasmus8 <arasmus8@gmail.com>
2020-08-04 12:39:04 -0600

    fix(ui): card image

--- src/main/java/dregsmod/cards/uncommon/LaserFocus.java
+++ src/main/java/dregsmod/cards/uncommon/LaserFocus.java
@@ -20,7 +20,7 @@ public class LaserFocus extends AbstractCurseHoldingCard {
 
     public static final String ID = DregsMod.makeID(LaserFocus.class.getSimpleName());
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG = makeCardPath("Skill.png");
+    public static final String IMG = makeCardPath("LaserFocus.png");
 // Must have an image with the same NAME as the card in your image folder!
 
 // /TEXT DECLARATION/
