e03b86d3b9a2281b9aafa52c11a6d805cebef228
arasmus8 <arasmus8@gmail.com>
2022-07-27 12:48:25 -0600

    fix: prevent crash if a non-card adds Mark

--- src/main/java/dregsmod/patches/powers/TriggerMarksPatch.java
+++ src/main/java/dregsmod/patches/powers/TriggerMarksPatch.java
@@ -2,10 +2,8 @@ package dregsmod.patches.powers;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.common.LoseHPAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.watcher.MarkPower;
 import dregsmod.cards.uncommon.Pinprick;
@@ -16,7 +14,7 @@ import dregsmod.cards.uncommon.Pinprick;
 )
 public class TriggerMarksPatch {
     public static void Postfix(MarkPower _instance, AbstractCard card) {
-        if(card.cardID.equals(Pinprick.ID)) {
+        if (card != null && card.cardID.equals(Pinprick.ID)) {
             AbstractDungeon.actionManager.addToBottom(new LoseHPAction(
                     _instance.owner,
                     null,
